17027785ba3e9ed3f07e89e894c799ef2c8e0cba
Kelton <keltonrpatricio@gmail.com>
2021-02-13 14:04:22 -0300

    before auth: hotfix

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -23,6 +23,9 @@
     </head>   
     <body class="font-sans antialiased">
         <div class="relative flex items-top justify-center min-h-screen bg-gray-100 dark:bg-gray-900 sm:items-center sm:pt-0">
+            <div class="min-h-screen bg-gray-100 dark:bg-gray-900">
+                @include('layouts.navigation')
+                </div>
             <div class="max-w-6xl mx-auto sm:px-5 lg:px-8">  
                 <div class="content">
                 @yield('content')
